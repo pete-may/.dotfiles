c4fbd298146244b7e70181867a7f30b982990855
Peter-May <peter.may@ibm.com>
2020-04-25 19:21:44 -0400

    test raspi install

--- raspi/raspi_install.sh
+++ raspi/raspi_install.sh
@@ -10,18 +10,12 @@ echo "Installing vim..."
 sudo apt-get install vim-nox -y
 
 echo "Linking files..."
-ln -s $HOME/.dotfiles/raspi/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/raspi/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/raspi/.zlogin $HOME/.zlogin
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
 
 echo "Setting up vim..."
 ln -s $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
-mkdir $HOME/.vim
+mkdir -p $HOME/.vim
 ln -s $HOME/.dotfiles/vim/colors $HOME/.vim/colors 
-
-echo "Install YCM dependencies"
-sudo apt-get install build-essential cmake python3-dev
-cd $HOME/.dotfiles/vim/bundle/YouCompleteMe
-python3 install.py
 
 chsh -s /bin/zsh

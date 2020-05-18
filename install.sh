9bcf22bae3d60fecddfa74735a6882ada1ec462c
Dev <dev@Peters-MacBook-Pro.local>
2020-05-17 22:21:06 -0400

    added alacritty config

--- install.sh
+++ install.sh
@@ -3,18 +3,22 @@
 if [ -f ~/.bash_profile ]; then
     mv ~/.bash_profile ~/.bash_profile.bak
 fi
+
 if [ -f ~/.bashrc ]; then
     mv ~/.bashrc ~/.bashrc.bak
 fi
+
 if [ -f ~/.inputrc ]; then
     mv ~/.inputrc ~/.inputrc.bak
 fi
+
 if [ -f ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc.bak
 fi
 
-ln -s ~/.dotfiles/bash/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/.vim ~/.vim
 
-source ~/.bash_profile
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc

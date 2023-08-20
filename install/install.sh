0d74566385bb3081a15f2fd73f5925f5d5798b45
Pete May <pmay804@gmail.com>
2023-08-19 23:45:14 -0400

    add casks to install

--- install/install.sh
+++ install/install.sh
@@ -20,18 +20,33 @@ if [ -f ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc.bak
 fi
 
-ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/.vim ~/.vim
-
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
-# kitty
-mkdir -p ~/.config/kitty
-ln -s ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+mkdir -p ~/.config/wezterm
+mkdir -p ~/.config/nvim
+
+ln -s ~/.dotfiles/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
+ln -s ~/.dotfiles/nvim/init.lua  /.config/nvim/init.lua
+
+./install_brew.sh
+
+brew install --cask 1password
+brew install --cask alfred
+brew install --cask wezterm
+brew install --cask spotify
+brew install --cask slack
+brew install --cask firefox
+brew install --cask obsidian
+brew install --cask alt-tab
+brew install --cask bettertouchtool
+# brew install --cask anki
 
 brew install fzf
 brew install bat
 brew install ripgrep
 brew install fd
 brew install python3
-
+brew install neovim
+brew install yabai
+brew install skhd
+brew install jq
